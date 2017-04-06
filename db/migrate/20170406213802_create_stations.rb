d7f1b9a597884dc4e97d5a0e1ba590410b734e81
taketimeasafriend <taketimeasafriend@gmail.com>
2017-04-07 05:40:52 +0800

    add station

---
+++ db/migrate/20170406213802_create_stations.rb
@@ -0,0 +1,11 @@
+class CreateStations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stations do |t|
+      t.string :sta_name
+      t.string :sta_enname
+      t.string :sta_code
+
+      t.timestamps
+    end
+  end
+end
