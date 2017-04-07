d36c7e53f2aaac95cb38c8df467cc9ba349f3af4
taketimeasafriend <taketimeasafriend@gmail.com>
2017-04-07 08:26:19 +0800

    add train and reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406213802) do
+ActiveRecord::Schema.define(version: 20170407001502) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "juhe_id"
@@ -23,6 +23,21 @@ ActiveRecord::Schema.define(version: 20170406213802) do
     t.index ["juhe_id"], name: "index_cities_on_juhe_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.string   "booking_code"
+    t.integer  "train_id"
+    t.string   "seat_number"
+    t.integer  "user_id"
+    t.string   "customer_name"
+    t.string   "customer_phone"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["booking_code"], name: "index_reservations_on_booking_code"
+    t.index ["seat_number"], name: "index_reservations_on_seat_number"
+    t.index ["train_id"], name: "index_reservations_on_train_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "stations", force: :cascade do |t|
     t.string   "sta_name"
     t.string   "sta_enname"
@@ -30,6 +45,13 @@ ActiveRecord::Schema.define(version: 20170406213802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sta_name"], name: "index_stations_on_sta_name"
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["number"], name: "index_trains_on_number"
   end
 
 end
